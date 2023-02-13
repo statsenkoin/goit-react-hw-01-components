e6386de16275925b7f6888edfe46553197dfd51d
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-13 16:04:01 +0200

    Feature/ add random color

--- src/components/Statistics/StatisticsData.jsx
+++ src/components/Statistics/StatisticsData.jsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
+import getRandomHexColor from 'js/randomColor';
 
 export default function StatisticsData({ id, label, percentage }) {
   return (
-    <li className={css.item} key={id}>
+    <li
+      className={css.item}
+      style={{ backgroundColor: getRandomHexColor() }}
+      key={id}
+    >
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
     </li>
