d6e4efa3961e4cd0cba3e40f211b132a2a1182eb
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-14 23:57:30 +0200

    Feature/ add color constants

---
+++ src/constants/theme.js
@@ -0,0 +1,13 @@
+export const themeLight = {
+  colors: {
+    textMain: '#505050',
+    textSecondary: '#808080',
+    textLight: '#fafafa',
+
+    backgroundMain: '#fafafa',
+    backgroundSecondary: '#f0f0f0',
+
+    border: '#e0e0e0',
+    shadow: '#00000033',
+  },
+};
