d90c4468db7ba8d0220a96e190c4b3be5a5de1f0
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-15 19:06:23 +0200

    Feature/ add TransactionsHistory.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import user from 'dataBase/user.json';
 import data from 'dataBase/data.json';
 import friends from 'dataBase/friends.json';
+import transactions from 'dataBase/transactions.json';
 
 import { AppContainer } from './App.styled';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions}></TransactionHistory>
     </AppContainer>
   );
 };
