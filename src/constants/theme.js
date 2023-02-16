5da5bb1e9ec5edf766325338ec02815ec52bbd58
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-16 21:57:39 +0200

    Update theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,4 +1,4 @@
-export const theme = {
+export const themeLight = {
   colors: {
     textMain: '#505050',
     textSecondary: '#808080',
@@ -8,7 +8,28 @@ export const theme = {
     backgroundSecondary: '#f0f0f0',
 
     border: '#e0e0e0',
-    shadow: `rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px,
-    rgb(0 0 0 / 12%) 0px 1px 8px 0px`,
+    shadow: `rgb(0 0 0 / 20%) 0px 3px 3px -2px, 
+             rgb(0 0 0 / 14%) 0px 3px 4px 0px,
+             rgb(0 0 0 / 12%) 0px 1px 8px 0px`,
   },
 };
+
+export const themeDark = {
+  colors: {
+    textMain: '#afafaf',
+    textSecondary: '#5f5f5f',
+    textLight: '#fafafa',
+
+    backgroundMain: '#222222',
+    backgroundSecondary: '#2f2f2f',
+
+    border: '#3f3f3f',
+    shadow: `rgb(256 256 256 / 20%) 0px 3px 3px -2px, 
+             rgb(256 256 256 / 14%) 0px 3px 4px 0px,
+             rgb(256 256 256/ 12%) 0px 1px 8px 0px`,
+  },
+};
+
+const isThemeLight = true;
+// const isThemeLight = false;
+export const theme = isThemeLight ? themeLight : themeDark;
